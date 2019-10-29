435e0be7138fa7161e5ae518cc5c64927c3774d6
ecustovic <emina.custovic@edu.fit.ba>
2019-10-29 21:46:18 +0100

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+    validates :title, presence: true
+    validates :description,  presence: true, length: {minimum: 3}
+    validates :image_url, presence: true
+    validates_numericality_of :price, :greater_than_or_equal_to => 0.01
+    validates :image_url, allow_blank: true, format: { 
+        with: %r{\.(gif\jpg\png)\z}i,
+        message: 'must be a URL for GIF, JPG or PNG.'
+     }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,19 @@ Product.delete_all
 Product.create!(title: 'Build Chatbot Interactions',
   description:
     %{<p>
-      <em> eshkdfksjskcf </em>
-        dkjasldjsykfcdxcf.lfv
-        kfmvl.dxmflvrldg
+      <em> Responsive, Intuitive Interfaces </em>
+        with Ruby The next step in ...
         </p>},
-  image_url: 'coca_cola.jpg',
+  image_url: 'dpchat.jpg',
+  price: 3.70)
+
+
+  Product.create!(title: 'Programming Crystal',
+  description:
+    %{<p>
+      <em> Create High-Performance, Safe, Concurrent </em>
+        Apps Crytal is for Ruby ...
+        </p>},
+  image_url: 'crystal.jpg',
   price: 3.70)
 # . . . 
