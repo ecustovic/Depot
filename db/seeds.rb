9c5b5f55c0e0848d58e618e905e41de798e01a17
ecustovic <emina.custovic@edu.fit.ba>
2019-10-29 22:39:50 +0100

    assertations

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,4 @@ Product.create!(title: 'Build Chatbot Interactions',
         </p>},
   image_url: 'dpchat.jpg',
   price: 3.70)
-
-
-  Product.create!(title: 'Programming Crystal',
-  description:
-    %{<p>
-      <em> Create High-Performance, Safe, Concurrent </em>
-        Apps Crytal is for Ruby ...
-        </p>},
-  image_url: 'crystal.jpg',
-  price: 3.70)
 # . . . 
