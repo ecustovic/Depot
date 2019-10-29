4c2ddb1f920ae835734f3cbc546d963896c4aaab
ecustovic <emina.custovic@edu.fit.ba>
2019-10-29 20:37:47 +0100

    scaffold depot

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+Product.delete_all
+# . . .
+Product.create!(title: 'Build Chatbot Interactions',
+  description:
+    %{<p>
+      <em> eshkdfksjskcf </em>
+        dkjasldjsykfcdxcf.lfv
+        kfmvl.dxmflvrldg
+        </p>},
+  image_url: 'coca_cola.jpg',
+  price: 3.70)
+# . . . 
