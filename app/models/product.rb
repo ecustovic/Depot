49b74af1c39de62652bbc2c731f9288f471ecd13
ecustovic <emina.custovic@edu.fit.ba>
2019-10-30 13:57:34 +0100

    functional testing of controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,9 @@
 class Product < ApplicationRecord
-
-    validates :title, presence: true
-    validates :description,  presence: true, length: {minimum: 3}
-    validates :image_url, presence: true
-    validates_numericality_of :price, :greater_than_or_equal_to => 0.01
-    validates :image_url, allow_blank: true, format: { 
-        with: %r{\.(gif\jpg\png)\z}i,
-        message: 'must be a URL for GIF, JPG or PNG.'
-     }
-
-end
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)\Z}i, # <<<<<----
+      message: 'must be a URL for GIF, JPG, or PNG image.'
+    }
+  end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -42,7 +42,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
-
+    
     assert_redirected_to products_url
   end
 end
